b9e393bbd42af09617cf4743272e2dbbb8381a91
Renan Lucilio <renanlucilio@gmail.com>
2021-12-09 07:46:38 -0300

    change prop

--- src/app/modules/layout/components/layout/layout.component.ts
+++ src/app/modules/layout/components/layout/layout.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastService } from 'src/app/core/shared/services/services/toast.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  constructor(private readonly toast: ToastService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  showInDevelopment() {
+    this.toast.show('Em desenvolvimento');
   }
-
 }
